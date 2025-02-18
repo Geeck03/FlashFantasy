32a3b580a9dda3b259fe16d226bb125b510501ca
Ethan <tian01489@gmail.com>
2025-02-18 14:12:29 -0600

    Initial Commit

---
+++ FlashFantasy/FlashFantasyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashFantasyApp.swift
+//  FlashFantasy
+//
+//  Created by Ethan Jiang on 2/18/25.
+//
+
+import SwiftUI
+
+@main
+struct FlashFantasyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
